a2c2efd28023ffab8296cf91fde10b10554b7d49
Rocco <rokas.kran@gmail.com>
2019-11-28 17:53:06 +0200

    Add project files.

---
+++ IS_Project/Controllers/UserSubsystemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS_Project.Controllers
+{
+    public class UserSubsystemController : Controller
+    {
+        // GET: UserSubsystem
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        public ActionResult DeleteUser()
+        {
+            return View();
+        }
+    }
+}
