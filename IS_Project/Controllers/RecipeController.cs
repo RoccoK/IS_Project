b82c0faceb5f4212c61bc98f2a931ac97303fcb6
SidlauskasArminas <sidlauskasarminas@gmail.com>
2019-11-30 16:23:08 +0200

    Pridejau kalendoriu

---
+++ IS_Project/Controllers/RecipeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IS_Project.Models;
+using IS_Project.ViewModels;
+
+namespace IS_Project.Controllers
+{
+    public class RecipeController : Controller
+    {
+        hospitaldbContext ctx = new hospitaldbContext();
+        // GET: Recepy
+        public ActionResult Index()
+        {
+            Receptas[] a = ctx.Receptas.ToArray();
+            List<Receptas> fit = new List<Receptas>();
+            for (int i = 0; i < ctx.Receptas.Count(); i++)
+            {
+                a[i].FkPacientas = ctx.Pacientas.Find(a[i].FkPacientasId);
+                a[i].FkPacientas.PacientasNavigation = ctx.Vartotojas.Find(a[i].FkPacientas.PacientasId);
+                a[i].FkVaistas = ctx.Vaistas.Find(a[i].FkVaistasId);
+                fit.Add(a[i]);
+            }
+            List<Receptas> aaa = ctx.Receptas.ToList();
+            return View(fit);
+        }
+
+        // GET: Recepy/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Recepy/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Recepy/Create
+        [HttpPost]
+        public ActionResult Create(Receptas receptas)
+        {
+            try
+            {
+                ctx.Add(receptas);
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Recepy/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Recepy/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Recepy/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Recepy/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- IS_Project/Models/Receptas.cs
+++ IS_Project/Models/Receptas.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace IS_Project.Models
 {
     public partial class Receptas
     {
+        [DisplayName("Vaistas")]
         public int FkVaistasId { get; set; }
+        [DisplayName("Pacientas")]
         public int FkPacientasId { get; set; }
+        [DisplayName("Data")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Data { get; set; }
+        [DisplayName("Laikas")]
         public TimeSpan? Laikas { get; set; }
+        [DisplayName("Recepto nr.")]
         public int ReceptoNr { get; set; }
 
         public virtual Pacientas FkPacientas { get; set; }
         public virtual Vaistas FkVaistas { get; set; }
+
+        public IList<SelectListItem> VaistasList { get; set; }
+        public IList<SelectListItem> PacientasList { get; set; }
     }
 }
