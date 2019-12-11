bcfa0dfc2c915fbf431b65d386b02b7344d2aedd
Rocco <rokas.kran@gmail.com>
2019-12-11 18:27:16 +0200

    user registrations with roles

--- IS_Project/Controllers/UserSubsystemController.cs
+++ IS_Project/Controllers/UserSubsystemController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using IS_Project.Models;
+using IS_Project.ViewModels;
 
 namespace IS_Project.Controllers
 {
@@ -16,14 +17,105 @@ namespace IS_Project.Controllers
         {
             return View();
         }
-        public ActionResult AddUser()
+        public ActionResult PreAddUser()
         {
             return View();
+        }
+        public ActionResult AddDoctorUser()
+        {
+            User userView = new User();
+            return View(userView);
+        }
+        [HttpPost]
+        public ActionResult AddDoctorUser(User usr)
+        {
+
+            Vartotojas v = new Vartotojas();
+            v.Vardas = usr.Vardas;
+            v.Pavarde = usr.Pavarde;
+            v.Elpastas = usr.Pastas;
+            v.TelNr = usr.Telefonas;
+
+            Adresas adr = new Adresas();
+            adr.Miestas = usr.Miestas;
+            adr.Gatve = usr.Gatve;
+            adr.NamoNr = usr.Namas;
+            adr.PastoKodas = usr.PastoKodas;
+            v.Adresas = adr;
+
+            Daktaras dr = new Daktaras();
+            dr.Specializacija = (int)Specializacijos.Chirurgas;
+            dr.PradetaDirbti = DateTime.Now;
+            dr.Busena = (int)DarboBusenos.Dirbantis;
+            dr.DaktarasId = ctx.Vartotojas.Last().VartotojasId + 1;
+            v.Daktaras = dr;
+
+            ctx.Add(v);
+            ctx.SaveChanges();
+            return RedirectToAction("../");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    
+                }
+
+            }
+            catch
+            {
+                return View(usr);
+            }
+        }
+        public ActionResult AddPatientUser()
+        {
+            User userView = new User();
+            return View(userView);
+        }
+        [HttpPost]
+        public ActionResult AddPatientUser(User usr)
+        {
+            
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Vartotojas v = new Vartotojas();
+                    v.Vardas = usr.Vardas;
+                    v.Pavarde = usr.Pavarde;
+                    v.Elpastas = usr.Pastas;
+                    v.TelNr = usr.Telefonas;
+
+                    Adresas adr = new Adresas();
+                    adr.Miestas = usr.Miestas;
+                    adr.Gatve = usr.Gatve;
+                    adr.NamoNr = usr.Namas;
+                    adr.PastoKodas = usr.PastoKodas;
+                    v.Adresas = adr;
+
+                    Pacientas p = new Pacientas();
+                    p.GimimoData = usr.GimimoData;
+                    p.GimimoMiestas = usr.GimimoMiestas;
+                    p.GimLigoninėsPav = usr.GimimoLigoninesPav;
+                    p.DraudimoNr = usr.DraudimoNr;
+                    p.GimimoLaikas = new TimeSpan(12, 0, 0);
+                    v.Pacientas = p;
+
+                    ctx.Add(v);
+                    ctx.SaveChanges();
+                    //markeRepository.addMarke(collection);
+                }
+
+                return RedirectToAction("../");
+            }
+            catch
+            {
+                return View(usr);
+            }
         }
         public ActionResult DeleteUser(int id)
         {
             Vartotojas v = ctx.Vartotojas.Find(id);
-            ViewModels.User usr = new ViewModels.User();
+            User usr = new User();
             usr.id = v.VartotojasId;
             usr.Vardas = v.Vardas;
             usr.Pavarde = v.Pavarde;
@@ -32,14 +124,14 @@ namespace IS_Project.Controllers
             Adresas adr = ctx.Adresas.Find(id);
             usr.Miestas = adr.Miestas;
             usr.Gatve = adr.Gatve;
-            usr.Namas = adr.NamoNr.ToString();
-            usr.PastoKodas = adr.PastoKodas.ToString();
+            usr.Namas = adr.NamoNr;
+            usr.PastoKodas = adr.PastoKodas;
             Pacientas p = ctx.Pacientas.Find(id);
             usr.GimimoData = p.GimimoData.Date;
             return View(usr);
         }
         [HttpPost]
-        public ActionResult DeleteUser(int id, ViewModels.User usr)
+        public ActionResult DeleteUser(int id, User usr)
         {
             try
             {
@@ -67,9 +159,9 @@ namespace IS_Project.Controllers
         }
         public ActionResult ViewUserData()
         {
-            int id = 4;
+            int id = 5;
             Vartotojas v = ctx.Vartotojas.Find(id);
-            ViewModels.User usr = new ViewModels.User();
+            User usr = new User();
             usr.id = v.VartotojasId;
             usr.Vardas = v.Vardas;
             usr.Pavarde = v.Pavarde;
@@ -78,8 +170,8 @@ namespace IS_Project.Controllers
             Adresas adr = ctx.Adresas.Find(id);
             usr.Miestas = adr.Miestas;
             usr.Gatve = adr.Gatve;
-            usr.Namas = adr.NamoNr.ToString();
-            usr.PastoKodas = adr.PastoKodas.ToString();
+            usr.Namas = adr.NamoNr;
+            usr.PastoKodas = adr.PastoKodas;
             Pacientas p = ctx.Pacientas.Find(id);
             usr.GimimoData = p.GimimoData.Date;
             return View(usr);
@@ -92,7 +184,7 @@ namespace IS_Project.Controllers
         {
             //prefill edit form with data
             Vartotojas v = ctx.Vartotojas.Find(id);
-            ViewModels.User usr = new ViewModels.User();
+            User usr = new User();
             usr.id = v.VartotojasId;
             usr.Vardas = v.Vardas;
             usr.Pavarde = v.Pavarde;
@@ -101,15 +193,15 @@ namespace IS_Project.Controllers
             Adresas adr = ctx.Adresas.Find(id);
             usr.Miestas = adr.Miestas;
             usr.Gatve = adr.Gatve;
-            usr.Namas = adr.NamoNr.ToString();
-            usr.PastoKodas = adr.PastoKodas.ToString();
+            usr.Namas = adr.NamoNr;
+            usr.PastoKodas = adr.PastoKodas;
             Pacientas p = ctx.Pacientas.Find(id);
             usr.GimimoData = p.GimimoData.Date;
             return View(usr);
         }
         // skidaddle, skidoodle, your code is now a noodle
         [HttpPost]
-        public ActionResult EditUser(int id, ViewModels.User usr)
+        public ActionResult EditUser(int id, User usr)
         {
             try
             {
@@ -124,8 +216,8 @@ namespace IS_Project.Controllers
                 Adresas adr = ctx.Adresas.Find(id);
                 adr.Miestas = usr.Miestas;
                 adr.Gatve = usr.Gatve;
-                adr.NamoNr = Int32.Parse(usr.Namas);
-                adr.PastoKodas = Int32.Parse(usr.PastoKodas);
+                adr.NamoNr = usr.Namas;
+                adr.PastoKodas = usr.PastoKodas;
                 ctx.Update(adr);
 
                 Pacientas p = ctx.Pacientas.Find(id);
