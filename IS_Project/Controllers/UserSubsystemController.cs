852c24984d8bc35c31d06ef123a9a7a0525f0771
Rocco <rokas.kran@gmail.com>
2019-12-12 18:37:13 +0200

    fixes and send email

--- IS_Project/Controllers/UserSubsystemController.cs
+++ IS_Project/Controllers/UserSubsystemController.cs
@@ -7,6 +7,7 @@ using IS_Project.Models;
 using IS_Project.ViewModels;
 using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace IS_Project.Controllers
 {
@@ -19,9 +20,28 @@ namespace IS_Project.Controllers
         {
             return View();
         }
-        public ActionResult SendReminder()
+        public ActionResult SendEmail()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SendEmail(Email model)
+        {
+            if (ModelState.IsValid)
+            {
+                SmtpClient client = new SmtpClient("smtp.mail.com");
+                client.Credentials = new NetworkCredential("roccofakeaccount@mail.com", "rocco123");
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("roccofakeaccount@mail.com");
+                mailMessage.To.Add(model.ToEmail);
+                mailMessage.Subject = "Priminimas";
+                mailMessage.Body = model.Message;
+                client.Send(mailMessage);
+                return RedirectToAction("../");
+
+            }
+            return View(model);
         }
         public ActionResult PreAddUser()
         {
@@ -120,28 +140,30 @@ namespace IS_Project.Controllers
             usr.Gatve = adr.Gatve;
             usr.Namas = adr.NamoNr;
             usr.PastoKodas = adr.PastoKodas;
-            Pacientas p = ctx.Pacientas.Find(id);
-            usr.GimimoData = p.GimimoData.Date;
             return View(usr);
         }
         [HttpPost]
         public ActionResult DeleteUser(int id, User usr)
         {
-            try
+            ctx.Remove(ctx.Pacientas.Find(id));
+            ctx.Remove(ctx.Adresas.Find(id));
+            if (!(ctx.Vartotojas.Find(id) == null))
             {
-                ctx.Remove(ctx.Pacientas.Find(id));
-                ctx.Remove(ctx.Adresas.Find(id));
                 ctx.Remove(ctx.Vartotojas.Find(id));
-                ctx.Remove(ctx.Daktaras.Find(id));
-                ctx.SaveChanges();
-                return RedirectToAction("../");
-            }
-            catch
-            {
-                ModelState.AddModelError("id", "Toks vartotojas neegizstuoja");
 
-                return View();
             }
+            if (!(ctx.Daktaras.Find(id) == null))
+            {
+                ctx.Remove(ctx.Daktaras.Find(id));
+
+            }
+            if (!(ctx.Receptas.Find(id) == null))
+            {
+                ctx.Remove(ctx.Receptas.Find(id));
+            }
+
+            ctx.SaveChanges();
+            return RedirectToAction("../");
         }
 
         public ActionResult UserLookup(string vp)
@@ -155,7 +177,7 @@ namespace IS_Project.Controllers
         }
         public ActionResult ViewUserData()
         {
-            int id = 15;
+            int id = 10;
             if (Request.Form["Vardas"] != null)
             {
                 var vart = ctx.Vartotojas
@@ -184,7 +206,6 @@ namespace IS_Project.Controllers
         }
         public ActionResult EditUser(int id)
         {
-            //prefill edit form with data
             Vartotojas v = ctx.Vartotojas.Find(id);
             User usr = new User();
             usr.id = v.VartotojasId;
@@ -199,36 +220,26 @@ namespace IS_Project.Controllers
             usr.PastoKodas = adr.PastoKodas;
             return View(usr);
         }
-        // skidaddle, skidoodle, your code is now a noodle
         [HttpPost]
         public ActionResult EditUser(int id, User usr)
         {
-            try
-            {
-                Vartotojas v = ctx.Vartotojas.Find(id);
-                v.VartotojasId = usr.id;
-                v.Vardas = usr.Vardas;
-                v.Pavarde = usr.Pavarde;
-                v.Elpastas = usr.Pastas;
-                v.TelNr = usr.Telefonas;
-                ctx.Update(v);
+            Vartotojas v = ctx.Vartotojas.Find(id);
+            v.VartotojasId = usr.id;
+            v.Vardas = usr.Vardas;
+            v.Pavarde = usr.Pavarde;
+            v.Elpastas = usr.Pastas;
+            v.TelNr = usr.Telefonas;
+            ctx.Update(v);
 
-                Adresas adr = ctx.Adresas.Find(id);
-                adr.Miestas = usr.Miestas;
-                adr.Gatve = usr.Gatve;
-                adr.NamoNr = usr.Namas;
-                adr.PastoKodas = usr.PastoKodas;
-                ctx.Update(adr);
+            Adresas adr = ctx.Adresas.Find(id);
+            adr.Miestas = usr.Miestas;
+            adr.Gatve = usr.Gatve;
+            adr.NamoNr = usr.Namas;
+            adr.PastoKodas = usr.PastoKodas;
+            ctx.Update(adr);
 
-                ctx.SaveChanges();
-                return RedirectToAction("/ViewUserData");
-            }
-            catch
-            {
-                ModelState.AddModelError("id", "Toks vartotojas neegizstuoja");
-
-                return View();
-            }
+            ctx.SaveChanges();
+            return RedirectToAction("/ViewUserData");
         }
     }
 }
