91a74d936875fdc82a17c8806076888a560008af
ViliusVV <33135408+ViliusVV@users.noreply.github.com>
2019-12-18 03:24:34 +0200

    works

--- IS_Project/Controllers/EmailController.cs
+++ IS_Project/Controllers/EmailController.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using IS_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using MailKit;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace IS_Project.Controllers
 {
-    public class EmailController : Controller
+    public static class EmailController
     {
-        // GET: Email
-        public ActionResult Index()
+
+        static hospitaldbContext ctx = new hospitaldbContext();
+
+        public static void SendMailRegister(Registracija registracija)
         {
-            return View();
+            var vart = ctx.Vartotojas.Find(registracija.FkPacientasId);
+            var doc = ctx.Daktaras.Where(d => d.DaktarasId == registracija.FkDaktarasId).Include(d => d.DaktarasNavigation).FirstOrDefault();
+
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Ligoninės Sistema", "lisqrnd@gmx.com"));
+            message.To.Add(new MailboxAddress("Pacientas", vart.Elpastas));
+            message.Subject = "Registracijos patvirtinimas";
+
+            message.Body = new TextPart("plain")
+            {
+                Text =  "Jūs užsiregistravote ligoninės sitemoje\n" +
+                        "Registracijos duomenys:\n" +
+                        "Patekimo data: " + registracija.PateikimoData.ToString() +
+                        "\nVisito data: " + registracija.VisitoData.ToString() +
+                        "\nGydytojas: " + doc.DaktarasNavigation.Vardas + " " + doc.DaktarasNavigation.Pavarde + " " + ((Specializacijos)doc.Specializacija).ToString("g")
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("mail.gmx.com", 587);
+                
+
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("lisqrnd@gmx.com", "BigBang0258");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
 
     }
--- IS_Project/Models/Registracija.cs
+++ IS_Project/Models/Registracija.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +11,7 @@ namespace IS_Project.Models
 {
     public partial class Registracija
     {
-        [DisplayName("id")]
+        [DisplayName("Registracijos ID")]
         public int RegistracijaId { get; set; }
         [DisplayName("Pateikimo data")]
         public DateTime PateikimoData { get; set; }
@@ -18,13 +19,21 @@ namespace IS_Project.Models
         public int Busena { get; set; }
         [DisplayName("Kviesta greitoji")]
         public sbyte KviestaGreitoji { get; set; }
+        [DisplayName("Paciento ID")]
         public int FkPacientasId { get; set; }
-        [DisplayName("Daktaras")]
+        [DisplayName("Daktaro ID")]
         public int FkDaktarasId { get; set; }
         [DisplayName("Vizito data")]
         public DateTime VisitoData { get; set; }
 
+        [DisplayName("Daktaras")]
         public virtual Daktaras FkDaktaras { get; set; }
+        [DisplayName("Pacientas")]
         public virtual Pacientas FkPacientas { get; set; }
+
+        [NotMapped]
+        public IList<SelectListItem> ListPat { get; set; }
+        [NotMapped]
+        public IList<SelectListItem> ListDoc { get; set; }
     }
 }
